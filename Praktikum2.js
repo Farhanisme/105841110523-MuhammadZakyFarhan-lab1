c633b096227c7a1ac37e9ba88e305f7f7c72fa52
Farhanisme <149998931+Farhanisme@users.noreply.github.com>
2024-06-03 11:56:14 +0800

    tugas2

---
+++ Praktikum2.js
@@ -0,0 +1,9 @@
+function berubah() {
+
+    document.getElementById("ubahTeks").innerHTML = "TWICE!!";
+
+    document.getElementById("Audio").play();
+    
+    document.getElementById("Gambar").src= "Praktikum2-TZUYU.jpeg";
+    
+}
